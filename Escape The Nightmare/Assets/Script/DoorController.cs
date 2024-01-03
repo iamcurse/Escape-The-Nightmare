fcf97f4366ce027fb926bf8a2fb1c93e508077df
Mr.Curse <buschebus@gmail.com>
2024-01-03 05:58:34 +0100

    Level 4 Added
    - Level 4 Made By Nurbek

--- Escape The Nightmare/Assets/Script/DoorController.cs
+++ Escape The Nightmare/Assets/Script/DoorController.cs
@@ -112,22 +112,11 @@ public class DoorController : MonoBehaviour
             if (isChangeScene) {
                 if (sceneName != "") {
                     SceneManager.LoadScene(sceneName);
-                    Debug.Log("Enter Scene: " + sceneName);
                 } else {
                     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-                    Debug.Log("Enter Scene: " + NameFromIndex(SceneManager.GetActiveScene().buildIndex + 1));
                 }
             }
         }
-    }
-
-    private static string NameFromIndex(int buildIndex)
-    {
-        string path = SceneUtility.GetScenePathByBuildIndex(buildIndex);
-        int slash = path.LastIndexOf('/');
-        string name = path.Substring(slash + 1);
-        int dot = name.LastIndexOf('.');
-        return name.Substring(0, dot);
     }
 
     private void Start() {
