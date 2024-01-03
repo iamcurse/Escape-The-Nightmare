0449d122c97ed19575b340cbdfc289c2d1fc8a25
Mr.Curse <buschebus@gmail.com>
2024-01-03 22:24:00 +0100

    Change Project File Tree And Level Improved

--- Assets/Script/Level Script/Level2Event.cs
+++ Assets/Script/Level Script/Level2Event.cs
@@ -5,8 +5,6 @@ public class Level2Event : MonoBehaviour
 {
     public UnityEvent bottlePuzzle;
     private int _bottlePlace;
-    public int BottlePlace { get => _bottlePlace; set => _bottlePlace = value; }
-
     public void PlaceBottle() {
         _bottlePlace++;
         Debug.Log("Bottle Place: " + _bottlePlace);
